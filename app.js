a84716f9e0ce7480ab0d65ddcdf070307918f122
Anwer Hosen <nur.cnits@gmail.com>
2025-07-19 00:00:18 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let arr=["NUR","TUR","JAHID"];
+
+
+
+function conc(arr)
+{
+    let r="";
+
+    for(let i=0; i<arr.length;i++)
+    {
+        r=r+arr[i];
+
+       
+    }
+     return r;
+
+   
+}
+
+
+let p=conc(arr);
+console.log(p);
+
+
+
+
